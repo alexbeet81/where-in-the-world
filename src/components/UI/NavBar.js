8278f9aabe8019a02d5444f3f11a4b7ae11a32a4
Alex Pritchard <alexbeet1234@gmail.com>
2021-11-16 15:15:24 +0800

    installed reulgar svg icons package from fontawesome

--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import './NavBar.css';
 
 const NavBar = () => {
   return (
-    <div>This is the nav bar</div>
+    <div className="nav_bar__container">
+      <h1>Where in the world?</h1>
+      <div>
+        <FontAwesomeIcon className="nav_bar__moon" icon={faMoon} />
+        <p>dark mode</p>
+      </div>
+    </div>
   )
 };
 
